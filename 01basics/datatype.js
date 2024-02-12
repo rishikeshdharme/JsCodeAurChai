ef0ab3b6107ec8f5eb06148cd52430e26069b0b2
rishikeshdharme <145563510+rishikeshdharme@users.noreply.github.com>
2024-02-12 09:33:26 +0000

    datatype

---
+++ 01basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all Js Code as newer version
+
+//alert(3+3) // we are not using this bcz we are using Node.js not browser
+
+//console.log(3+3);
+
+let Name = "Rishi";
+let age = 21;
+let isman = false;
+let isready;
+let AreReady = null;
+
+console.log(Name);
+console.table([Name,age,isman,isready,AreReady]);
+
+console.log(typeof isready);   // ouput- Undefined
+console.log(typeof AreReady);  // output- Object(null)
