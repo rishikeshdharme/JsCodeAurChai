5c99e8a797d341c09fea96131738c356f5e35bf0
rishikeshdharme <145563510+rishikeshdharme@users.noreply.github.com>
2024-02-12 10:22:01 +0000

    conversion of datatype

---
+++ 01basics/ConversionOperation.js
@@ -0,0 +1,32 @@
+/*let nub = true;
+
+console.log(typeof nub);
+console.log(typeof(nub));
+
+let valueofnub = Number(nub);
+console.log(typeof valueofnub);
+console.log(valueofnub);  */
+
+// "33" = 33
+//  33abc = NaN(Number) 
+//  null = 0
+// undefined = NaN
+// true =1  false=0
+
+
+/*let sos = 1;
+
+console.log(typeof sos);
+let valueofsos = Boolean(sos);
+console.log(typeof(valueofsos));
+console.log(valueofsos);  */
+
+// "rishi" => true
+// "" => false
+
+
+let someNumber = 33;
+
+let stringnumber = String(someNumber);
+console.log(stringnumber);
+console.log(typeof(stringnumber));
