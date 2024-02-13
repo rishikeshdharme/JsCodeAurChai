07d244588824383349d6e1a919958254d8057ba2
rishikeshdharme <145563510+rishikeshdharme@users.noreply.github.com>
2024-02-13 00:18:04 +0000

    memory

--- 01basics/TypeofDatatypes.js
+++ 01basics/TypeofDatatypes.js
@@ -4,14 +4,14 @@
 //Number , String ,Boolean, null, undefined,BigInt,Symbol;
 
 let BigInt = 1234567890078954n;
-console.log(BigInt);
-console.log(typeof(BigInt));
+//console.log(BigInt);
+//console.log(typeof(BigInt));
 
 let Id = Symbol('981');
 
 let CId = Symbol('981');
 
-console.log(Id==CId);
+//console.log(Id==CId);
 
 
 
@@ -38,8 +38,35 @@ let MyFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof(heroes));
+//console.log(typeof(heroes));
 
-console.log(typeof(Myobj));
+//console.log(typeof(Myobj));
 
-console.log(typeof(MyFunction));
+//console.log(typeof(MyFunction));
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(primitive datatype)   ,  Heap(Non Primitive datatype)
+
+let name1 = "Rishi";
+let name2 = name1;
+ 
+name2="Rishikesh";
+console.log(name2);
+console.log(name1); //cannot change the value bcz in case of stack we get copy of value
+
+
+let fetchdata =
+{
+    name:"rishu",
+    email:"rishudharme@gmail.com",
+    upi:"rishu.upi"
+}
+
+let fetchdata2 = fetchdata;
+console.log(fetchdata2);
+
+fetchdata2.upi="rishidhame.api";
+console.log(fetchdata2.upi);
+console.log(fetchdata.upi);    // this value are change bcz case of heap it give a refrence that is the value was change
